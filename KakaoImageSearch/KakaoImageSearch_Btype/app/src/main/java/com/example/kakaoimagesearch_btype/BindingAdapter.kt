de40ad7fb026f796e440be46deece1155d670705
khjgggg <guswjd7@gmail.com>
2023-09-19 00:02:54 +0900

    [FEAT] : ⚙️(main): 아이템 날짜/시간표시

--- KakaoImageSearch/KakaoImageSearch_Btype/app/src/main/java/com/example/kakaoimagesearch_btype/BindingAdapter.kt
+++ KakaoImageSearch/KakaoImageSearch_Btype/app/src/main/java/com/example/kakaoimagesearch_btype/BindingAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.kakaoimagesearch_btype
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import java.text.SimpleDateFormat
 
 @BindingAdapter("imgRes")
 fun imgRes(imageView: ImageView, url:String) {
@@ -10,4 +12,26 @@ fun imgRes(imageView: ImageView, url:String) {
     Glide.with(imageView.context)
         .load(url)
         .into(imageView)
+}
+
+@BindingAdapter("setDisplayText")
+fun setDisplayText(textView: TextView, text: String) {
+    textView.text = "[image] $text"
+}
+
+@BindingAdapter("setTextDatetime")
+fun setTextDatetime(textView: TextView, text: String) {
+    textView.text = dateformat(text)
+}
+
+fun dateformat(inputDateString: String): String {
+    // 입력 문자열을 Date 객체로 파싱합니다.
+    val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    val date = inputFormat.parse(inputDateString)
+
+    // 출력 형식을 정의하고 원하는 형식으로 포맷합니다.
+    val outputFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+    // 포맷팅된 문자열을 얻습니다.
+    return outputFormat.format(date)
 }
